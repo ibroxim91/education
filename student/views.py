2eafda83e04288ddc955be6ad42272fe6b442361
ibrohim <ibroxim.rashidov@yandex.ru>
2023-12-06 14:52:59 +0500

    All files

--- student/views.py
+++ student/views.py
@@ -4,6 +4,5 @@ from django.http import JsonResponse
 # Create your views here.
 
 def home(request):
-    d = {"id":1,"name":"Olim","last_name":"Olimov","age":21,"phone":998991234567}
-    return JsonResponse(d)
+    return render(request, "index.html")
 
